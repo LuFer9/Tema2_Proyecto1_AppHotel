da533c3237a1b3aac784f5316be34dde1f90aefd
Salvador <Salvador@DESKTOP-3G9C619>
2021-11-03 18:12:08 +0100

    Primer diseño de la ventana, sin CSS

--- src/apphotel/AppHotelReservaHabitacionesViewController.java
+++ src/apphotel/AppHotelReservaHabitacionesViewController.java
@@ -7,7 +7,16 @@ package apphotel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 /**
@@ -22,6 +31,38 @@ public class AppHotelReservaHabitacionesViewController implements Initializable 
      */
     
     private Pane rootPrincipalView;
+    @FXML
+    private TextField textFieldDNI;
+    @FXML
+    private TextField textFieldNombre;
+    @FXML
+    private TextField textFieldDireccion;
+    @FXML
+    private TextField textFieldLocalidad;
+    @FXML
+    private ComboBox<?> comboBoxProvincia;
+    @FXML
+    private DatePicker datePickerLlegada;
+    @FXML
+    private DatePicker datePickerSalida;
+    @FXML
+    private Spinner<?> spinnerNumHab;
+    @FXML
+    private ComboBox<?> comboBoxTipoHab;
+    @FXML
+    private CheckBox checkBoxFumador;
+    @FXML
+    private Button buttonAceptar;
+    @FXML
+    private Button buttonCancelar;
+    @FXML
+    private Button buttonLimpiar;
+    @FXML
+    private RadioButton radioButtonAlojamiento;
+    @FXML
+    private RadioButton radioButtonMediaPension;
+    @FXML
+    private RadioButton radioButtonPensionCompleta;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -30,6 +71,18 @@ public class AppHotelReservaHabitacionesViewController implements Initializable 
     
     public void setRootPrincipalView(Pane rootPrincipalView){
         this.rootPrincipalView = rootPrincipalView;
+    }
+
+    @FXML
+    private void onActionButtonAceptar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionButtonLimpiar(ActionEvent event) {
     }
     
 }
